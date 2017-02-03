89381357a388cb4451bcc0d979f5dc482b312526
Laurent <laurent@lo-arch.localdomain>
2017-02-03 16:30:38 -0500

    added polybar and fixed colors

--- lemonbuddy/.config/lemonbuddy/launch.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/zsh
-
-# Terminate already running bar instances
-killall -q lemonbuddy
-
-lemonbuddy top &
-
---
+++ polybar/.config/polybar/launch.sh
@@ -0,0 +1,9 @@
+#!/bin/zsh
+
+# Terminate already running bar instances
+killall -q polybar
+
+while pgrep -x polybar >/dev/null; do sleep 1; done
+
+polybar top &
+
